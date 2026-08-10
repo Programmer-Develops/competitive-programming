52e7e5cb6a2b2939608a9fe96dc9b53491c8d88c
Programmer-Develops <shachipandya29@gmail.com>
2026-08-10 09:59:16 +0530

    Increasing and decreasing from user input integer

--- java-intro/intro2.java
+++ java-intro/intro2.java
@@ -1,35 +1,61 @@
 import java.util.*;
 
-class Calc {
+// Calculator
+// class Calc {
+//     public static void main(String[] args) {
+//         Scanner Input = new Scanner(System.in);
+
+//         int X = Input.nextInt();
+//         int Y = Input.nextInt();
+
+//         System.out.println("Sum = " + Add(X,Y));
+//         System.out.println("Multiplication = " + Mul(X,Y));
+//         Sub(X,Y);
+//         Div(X,Y);
+//     }
+
+//     static int Add(int X, int Y) {
+//         int Z = X + Y;
+//         return Z;
+//     }
+
+//     static void Sub(int X, int Y) {
+//         int Z = X - Y;
+//         System.out.println("Subtraction = " + Z);
+//     }
+
+//     static int Mul(int X, int Y) {
+//         int Z = X * Y;
+//         return Z;
+//     }
+
+//     static void Div(int X, int Y) {
+//         int Z = X / Y;
+//         System.out.println("Division = " + Z);
+//     }
+// }
+
+
+// Increasing/Decreasing Function
+class AD {
     public static void main(String[] args) {
         Scanner Input = new Scanner(System.in);
-
         int X = Input.nextInt();
-        int Y = Input.nextInt();
-
-        System.out.println("Sum = " + Add(X,Y));
-        System.out.println("Multiplication = " + Mul(X,Y));
-        Sub(X,Y);
-        Div(X,Y);
+        Increasing(X);
+        Decreasing(X);
     }
 
-    static int Add(int X, int Y) {
-        int Z = X + Y;
-        return Z;
+    public static void Increasing(int X) {
+        for(int i=1; i <= X; i++)
+        {
+            System.out.println("Increasing => " + i);
+        }
     }
 
-    static void Sub(int X, int Y) {
-        int Z = X - Y;
-        System.out.println("Subtraction = " + Z);
-    }
-
-    static int Mul(int X, int Y) {
-        int Z = X * Y;
-        return Z;
-    }
-
-    static void Div(int X, int Y) {
-        int Z = X / Y;
-        System.out.println("Division = " + Z);
+    public static void Decreasing(int X) {
+        for(int i=X; i >= 1; i--)
+        {
+            System.out.println("Decreasing => " + i);
+        }
     }
 }
