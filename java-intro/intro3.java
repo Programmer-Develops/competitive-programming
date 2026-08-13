3593d87f605ac8bda4efa695583f777fbc23a313
Programmer-Develops <shachipandya29@gmail.com>
2026-08-13 13:02:47 +0530

    Refactor factorial implementation and add recursive sum calculation with input handling

--- java-intro/intro3.java
+++ java-intro/intro3.java
@@ -2,31 +2,64 @@ import java.util.*;
 
 // factorial through recursion in java
 
-class rf {
+// class rf {
+//     public static void main(String[] args) {
+//         Scanner Input = new Scanner(System.in);
+//         int facNum = Input.nextInt();
+
+//         int result = factorial(facNum);
+//         System.out.println(result);
+//     }
+
+//     public static int factorial(int num) {
+//         // with recursion
+//         if(num == 0 || num == 1)
+//         {
+//             return 1;
+//         }
+//         return num * factorial(num - 1);
+
+//         // for(int i=1; i <= num; i++)
+//         // {
+//         //     int fact = 1;
+//         //     for(int j=1; j <= i; j++)
+//         //     {
+//         //         fact = fact * j;
+//         //     }
+//         //     System.out.println("Factorial of " + i + " is => " + fact);
+//         // }
+//     }
+// }
+
+
+// Sum with recursion in java
+
+class ra {
     public static void main(String[] args) {
         Scanner Input = new Scanner(System.in);
-        int facNum = Input.nextInt();
+        ArrayList<Integer> arr = new ArrayList<>();
+        int n = Input.nextInt();
 
-        int result = factorial(facNum);
-        System.out.println(result);
+        for(int i = 1; i <= n; i++) {
+            int ele = Input.nextInt();
+            arr.add(ele);
+        }
+        System.out.println(asum(arr));
     }
 
-    public static int factorial(int num) {
+    public static int asum(ArrayList<Integer> arr) {
         // with recursion
-        if(num == 0 || num == 1)
-        {
-            return 1;
+        if(arr.size() == 0) {
+            return 0;
         }
-        return num * factorial(num - 1);
+        int lastElement = arr.get(arr.size() - 1);   // get the last element of the array
+        arr.remove(arr.size() - 1); // remove the last element of the array
+        return lastElement + asum(arr); // call the function again with the remaining elements of the array
 
-        // for(int i=1; i <= num; i++)
-        // {
-        //     int fact = 1;
-        //     for(int j=1; j <= i; j++)
-        //     {
-        //         fact = fact * j;
-        //     }
-        //     System.out.println("Factorial of " + i + " is => " + fact);
+        // int sum = 0;
+        // for(int i=0; i < arr.size(); i++) {
+        //     sum += arr.get(i);
         // }
+        // return sum;
     }
 }
