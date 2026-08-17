a4a466c5ae2a53c8fb97fb35950f1d388e71b044
Programmer-Develops <shachipandya29@gmail.com>
2026-08-17 09:45:11 +0530

    counting integers in an input from user

---
+++ java-intro/intro4.java
@@ -0,0 +1,18 @@
+import java.util.*; 
+class cd {
+    public static void main(String[] args) {
+        Scanner Input = new Scanner(System.in);
+        int n = Input.nextInt();
+        System.out.println(countd(n));
+    }
+
+    public static int countd(int a) {
+        int count = 0;
+        // count integers in input a
+        while(a != 0) {
+            count += 1;
+            a = a/10;
+        }
+        return count;
+    }
+}
